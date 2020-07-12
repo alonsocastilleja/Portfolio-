c4f0e3bee0cdbb30330cdea683fd7a99ab06c18d
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-07-12 10:39:00 -0400

    added about section and styling changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,14 +12,31 @@ class About extends Component {
                     </div> 
                     <article className="about__text">
                         <h2 className="about__heading"><span className="char__font">&#60;</span><span className="span__underline-red"> About Me</span><span className="char__font"> /&#62; </span></h2>
-                        <p>Hello I am a hard working and passionate software developer </p>
+                        <p className="about__description">Hello, I am a hard working and passionate <span className="span__underline-red">software developer.</span> I enjoy the process of being 
+                        stuck on a problem and using  <span className="char__font-about">&#123;</span> Google <span className="char__font-about">&#125; </span>
+                        to solve it. I enjoy seeing my ideas and thoughts come to life through the use of different technologies.
+                        I am looking forward to learning more technologies and languages for the span of 
+                        my career!
+                        </p>
+                        <div className="about__quote">
+                            <p>"Greatness exists in all of us"</p>
+                            <p>- Will Smith</p>
+                        </div>
                     </article>
-                    <Sidebar />
-                </section>
+                    <figure className="about__photo">
+                        <img src="alonso_me.jpg" 
+                             alt="Alonso Castilleja is awesome!"
+                             height="300px"
+                             width="300px"
+                             ></img>
+                        <button className="about__btn"><a href="assets/resume.pdf" target="_blank">Resume</a></button>
+                    </figure>
 
-                <div class="arrow">
+                    <Sidebar /> 
+                    <div class="arrow">
                         <span></span>
-                </div>
+                    </div>
+                </section>
             </div>
         );
     }
