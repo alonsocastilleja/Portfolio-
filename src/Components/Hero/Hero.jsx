04ad3ab653e28f7d057f5ffbb2ec3388dc1d9c46
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-07-14 21:31:00 -0400

    fixed a color contrast issue

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = (props) => {
 
                     <div className="hero__text"> 
                         <h1 className="hero__name-hello">Hello, my name is</h1>
-                        <h2 className="hero__name">Alonso <span className="span__underline-red">Castilleja</span><span className="span__style-red">.</span></h2>
+                        <h2 className="hero__name">Alonso <span className="span__underline-text">Castilleja</span><span className="span__style-red">.</span></h2>
                         <p className="hero__subtitle"><span className="char__font">&#60;</span>  Software Developer <span className="char__font"> /&#62; </span></p>
                         <button className="hero__btn"><a href="/resume.pdf" target="_blank">Resume</a></button>
                     </div>
