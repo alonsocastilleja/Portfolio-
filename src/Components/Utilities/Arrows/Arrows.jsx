d5d63b6340fa33b492fa9d039b605658cdc3fd12
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-07-16 09:03:20 -0400

    changed the tab icon and description

--- src/Components/Utilities/Arrows/Arrows.jsx
+++ src/Components/Utilities/Arrows/Arrows.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-scroll";
 import './Arrows.css';
 
 const Arrow = (props) => {
+    console.log(props)
         return (
             <div>
                 <Link to={props.down}
                       className='arrow bounce'
                       smooth={true}
                       spy={true}
+                      style={props.style}
+                      id={props.id}
                 >
                     <img src={props.arrow} alt="transition arrow"></img>
                 </Link>
