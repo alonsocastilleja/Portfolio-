a4251a9fe748ffc661fb8a33cbd110b10b037edf
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-07-16 12:14:07 -0400

    added project data

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './Projects.css';
 import Arrow from '../Utilities/Arrows/Arrows';
-
+import projects from '../data/projects.json';
 
 
 const Projects = (props) => {
         return (
             <div>
-                <section className="projects" id="projects">
-                    <div className="projects__list">
-                        {  
-                            <h2>Coming soon...</h2>
-                        }
-                    </div>
+                <section className="projects">
+                    
                     <Arrow down={props.down} arrow={props.arrow} />
                 </section>
             </div>
