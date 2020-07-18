c3aa5a0b0a862e73cf5615dbbac5e76318c75142
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-07-18 09:28:31 -0400

    styling changes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = (props) => {
             <div>
                 <section id="contact">
                     <div className="contact__title">
-                        <h2 className="contact__heading"><span className="char__font">&#60;</span> Send me a message <span className="char__font"> &#62; </span></h2>
-                        <p className="contact__subheading">Please fill out the form below and I will respond to your as soon as possible!</p>
+                        <h2 className="contact__heading"><span className="char__font">&#60;</span> Get in touch with me! <span className="char__font"> &#62; </span></h2>
+                        <p className="contact__subheading">Fill out the form below and I'll get back to you promptly. </p>
                     </div>
                     <div className="contact__container">
                         <form name="htmlform" method="post" action="https://formsubmit.co/castilleja.alonso@gmail.com">
@@ -15,7 +15,7 @@ const Contact = (props) => {
                                 <input type="text" name="first_name" placeholder="NAME" required></input>
                             </label>
                             <label for="email address">
-                                <input  type="email" name="email" placeholder="MAIL" required></input>
+                                <input  type="email" name="email" placeholder="EMAIL" required></input>
                             </label>
                             <label for="comments or questions">
                                 <textarea name="comments" placeholder="MESSAGE" required ></textarea>
