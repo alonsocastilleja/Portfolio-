5b7cae8e80f6515ad544452fc5e72db6a69f11f5
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-07-20 08:43:57 -0400

    final touches

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Arrow from '../Utilities/Arrows/Arrows';
 import projects from '../data/projects.json';
+import Carousel from 'react-elastic-carousel'
 import '../Projects/Projects.css';
 
 
@@ -11,35 +12,21 @@ const Projects = (props) => {
                         <div className="projects__heading">
                             <h2 className="projects__heading-font"><span className="char__font">[</span> Projects <span className="char__font">]</span></h2>
                         </div>
-                    <div className="projects__content">
-                        <div className="projects__card">
-                            <img src={projects[0].icon} 
-                                alt={projects[0].title}
-                                height={'100%'}
-                                width={'100%'}></img>
-                            <h2 className="projects__subheading">{projects[0].title}</h2>
-                            <button className="projects__btn"><a href={projects[0].link} rel='noopener noreferrer' target="_blank">Live Demo</a></button>
-                            <button className="projects__btn"><a href={projects[0].github} rel='noopener noreferrer' target="_blank">Github</a></button>
-                        </div>
-                        <div className="projects__card">
-                            <img src={projects[1].icon} 
-                                alt={projects[1].title}
-                                height={'100%'}
-                                width={'100%'}></img>
-                            <h2 className="projects__subheading">{projects[1].title}</h2>
-                            <button className="projects__btn"><a href={projects[1].link} rel='noopener noreferrer' target="_blank">Live Demo</a></button>
-                            <button className="projects__btn"><a href={projects[1].github} rel='noopener noreferrer' target="_blank">Github</a></button>
-                        </div>
-                        <div className="projects__card">
-                            <img src={projects[2].icon} 
-                                alt={projects[2].title}
-                                height={'100%'}
-                                width={'100%'}></img>
-                              <h2 className="projects__subheading">{projects[2].title}</h2>
-                            <button className="projects__btn"><a href={projects[2].link} rel='noopener noreferrer' target="_blank">Live Demo</a></button>
-                            <button className="projects__btn"><a href={projects[2].github} rel='noopener noreferrer' target="_blank">Github</a></button>
-                        </div>
-                    </div>
+                        <Carousel className={'projects__content'} pagination={false} itemsToShow={3}>
+                            {
+                                projects.map((card) => {
+                                    return(
+                                        <div className="projects__card">
+                                            <img alt={card.title} src={card.icon} height={'100%'} width={'100%'}></img>
+                                            <h2 className="projects__subheading">{card.title}</h2>
+                                            <button class="projects__btn"><a href={card.link} target="_blank" rel={'noopener noreferrer'}>Live Demo</a></button>
+                                            <button class="projects__btn"><a href={card.github} target="_blank" rel={'noopener noreferrer'}>Github</a></button>
+                                        </div>
+                                        
+                                    )
+                                })
+                            }
+                        </Carousel>
                    
                     <Arrow down={props.down} arrow={props.arrow} />
             </section>
